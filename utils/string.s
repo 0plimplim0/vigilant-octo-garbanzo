537594ba73e08def5cf3f0fa0a4a586b56d97fc3
0plimplim0 <cesarcortes5to@gmail.com>
2026-06-16 00:54:27 -0600

    Add strlen for string utils

--- utils/string.s
+++ utils/string.s
@@ -8,35 +8,53 @@ strcmp:
   mov rbp, rsp
   
   xor ecx, ecx      # iterator
-.loop:
+.strcmp_loop:
   cmp rdx, 0
-  je .no_limit
+  je .strcmp_no_limit
   cmp rcx, rdx
-  jb .no_limit
+  jb .strcmp_no_limit
   xor eax, eax
-  jmp .epilogue
-.no_limit:
+  jmp .strcmp_epilogue
+.strcmp_no_limit:
   cmp byte ptr [rdi+rcx], 0
-  je .strend
+  je .strcmp_strend
   xor r8, r8
   mov r8b, byte ptr [rsi+rcx]
   cmp byte ptr [rdi+rcx], r8b
-  jne .end0
+  jne .strcmp_end0
   inc ecx
-  jmp .loop
+  jmp .strcmp_loop
 
   # Epilogue
-.epilogue:
+.strcmp_epilogue:
   mov rsp, rbp
   pop rbp
   ret
 
-.strend:
+.strcmp_strend:
   cmp byte ptr [rsi+rcx], 0
-  jne .end0
+  jne .strcmp_end0
   xor eax, eax
-  jmp .epilogue
+  jmp .strcmp_epilogue
 
-.end0:
+.strcmp_end0:
   mov eax, -1
-  jmp .epilogue
+  jmp .strcmp_epilogue
+
+.global strlen
+strlen:
+  # strlen(rdi = str_start | rsi = limiter(default=0x0))
+  #Prologue
+  push rbp
+  mov rbp, rsp
+
+  xor eax, eax      # iterator
+.strlen_loop:
+  cmp byte ptr [rdi+rax], sil
+  je .strlen_end
+  inc eax
+  jmp .strlen_loop
+.strlen_end:
+  mov rsp, rbp
+  pop rbp
+  ret
